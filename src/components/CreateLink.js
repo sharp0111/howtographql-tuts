6d753de043814ea7139ac99fc1b19e0c406eb36c
Res Yeg <shaqk702@gmail.com>
2018-11-01 14:58:49 -0700

    add mutation to App

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
 
 class CreateLink extends Component {
   state = {
@@ -26,7 +39,9 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <button onClick={`... you'll implement this 🔜`}>Submit</button>
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+            {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
       </div>
     )
   }
