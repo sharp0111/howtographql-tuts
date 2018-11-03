d77aae285d308ce5609f68b31301779f99e4af3e
Res Yeg <shaqk702@gmail.com>
2018-11-03 12:54:27 -0700

    add subscription to entry file

--- src/index.js
+++ src/index.js
@@ -10,9 +10,22 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
 import { AUTH_TOKEN } from './constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
+})
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+      reconnect: true,
+      connectionParams: {
+        authToken: localStorage.getItem(AUTH_TOKEN),
+      }
+    }
 })
 
 const authLink = setContext((_, { headers }) => {
@@ -25,8 +38,17 @@ const authLink = setContext((_, { headers }) => {
     }
 })
 
+const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache()
 })
 
