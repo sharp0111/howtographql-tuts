b692cf97aa2ba87d36328ae14954547ecbcb2e3a
Res Yeg <shaqk702@gmail.com>
2018-11-03 12:19:36 -0700

    add more mutations and updating the store

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
-
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -11,12 +10,32 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `
 
 class LinkList extends Component {
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
   render() {
 
     return (
@@ -27,7 +46,9 @@ class LinkList extends Component {
                 const linksToRender = data.feed.links
                 return (
                     <div>
-                        {linksToRender.map(link => <Link key={link.id} link={link} />)}
+                        {linksToRender.map((link, index) => (
+                            <Link key={link.id} link={link} index={index} updateStoreAfterVote={this._updateCacheAfterVote}/>
+                        ))}
                     </div>
                 )
             }}
