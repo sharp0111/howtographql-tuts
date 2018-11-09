38a7f0f63a4abf848f430a0fe1562f7298b9c4f2
Res Yeg <shaqk702@gmail.com>
2018-11-08 17:25:34 -0800

    connect server and db with prisma bindings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
 let links = [{
     id: 'link-0',
@@ -34,6 +35,15 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://us1.prisma.sh/sergey-nam/database/dev',
+            secret: 'mysecret123',
+            debug: true,
+        })
+    })
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000!`))
