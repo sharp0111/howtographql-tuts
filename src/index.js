45192a414e409f0d8cdae130eba550556af27846
Res Yeg <shaqk702@gmail.com>
2018-11-09 15:39:23 -0800

    add Realtime GraphQL Subscriptions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
 
 let links = [{
     id: 'link-0',
@@ -14,7 +15,8 @@ let idCount = links.length
 const resolvers = {
     Query,
     Mutation,
-    AuthPayload
+    AuthPayload,
+    Subscription,
 }
 
 const server = new GraphQLServer({
