45520e8feb6ba5c9e5e2fc44ee2e9cf5b917d46b
Res Yeg <shaqk702@gmail.com>
2018-11-09 17:28:54 -0800

    add sorting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
 const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
 
 let links = [{
     id: 'link-0',
@@ -17,6 +18,7 @@ const resolvers = {
     Mutation,
     AuthPayload,
     Subscription,
+    Feed,
 }
 
 const server = new GraphQLServer({
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,4 @@
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
     const where = args.filter
     ? {
         OR: [
@@ -7,7 +7,26 @@ function feed(parent, args, context, info) {
         ],
       }
     : {}
-    return context.db.query.links({ where, skip: args.skip, first: args.first }, info)
+    // 1
+    const queriedLinks = await context.db.query.links(
+        { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+        `{ id }`,
+    )
+    // 2
+    const countSelectionSet = `
+    {
+        aggregate {
+        count
+        }
+    }
+    `
+    const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+
+    // 3
+    return {
+        count: linksConnection.aggregate.count,
+        linkIds: queriedLinks.map(link => link.id),
+    }
 }
   
 module.exports = {
