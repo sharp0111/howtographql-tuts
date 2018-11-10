b0c7d44bfd113349a1138f02dafe5463ac001ec5
Res Yeg <shaqk702@gmail.com>
2018-11-09 16:50:40 -0800

    add filtering capability

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,14 @@
 function feed(parent, args, context, info) {
-    return context.db.query.links({}, info)
-  }
+    const where = args.filter
+    ? {
+        OR: [
+          { url_contains: args.filter },
+          { description_contains: args.filter },
+        ],
+      }
+    : {}
+    return context.db.query.links({ where }, info)
+}
   
 module.exports = {
     feed,
